c4c14f94ba722d267bc2ce9be070e7257d9244b3
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-03-22 20:52:09 -0700

    Adding testing m file.

---
+++ testData/yuvDCT_Test.m
@@ -0,0 +1,44 @@
+% Read one frame on a cif image.
+height = 288;
+width = 352;
+[mov, rgbImg]= loadFileYuv('bus_cif.yuv',width, height, 1);
+clear mov;
+
+rgbImg = rgbImg(:,:,1);
+
+% Resize to some values that can be 
+scaledImg = imresize(rgbImg, [256 256]);
+
+% Substract 128 and turn into double type matrix
+scaledImg = double(scaledImg) - 128 ;
+
+dctDomain = double(zeros(256));
+inverseDctDomain = double(zeros(256));
+
+for r=1:8:256
+    for w=1:8:256
+        
+         % Apply 2D DCT
+         mb = dct(scaledImg(r: (r + 7), w:(w+7)));
+         mb = dct(mb')';
+         %mb(1) = mb(1) + 100;
+         
+         mb = abs(round(mb));
+         
+         % Zero out DCT high frequencies
+%          c = 0;
+%          for i=1:8
+%              mb(8-c:8,i) = 0;
+%              c = c + 1;
+%          end
+         
+         % Store the macroblock in the dctDomain matrix
+         dctDomain(r: (r + 7), w:(w+7)) = mb;
+         
+         % Attempt to reverse DCT to see effects
+%          inverseDctDomain(r: (r + 7), w:(w+7)) = idct( dctDomain(r: (r + 7), w:(w+7)) );
+         inverseDctDomain(r: (r + 7), w:(w+7)) = ( idct(idct(mb)')' + 128 );
+    end
+end
+inverseDctDomain = imresize(uint8(inverseDctDomain), [288 352]);
+
