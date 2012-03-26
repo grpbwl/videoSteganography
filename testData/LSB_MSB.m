ec30a167597b3e9a65d6845fd30b5577f365106d
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-03-26 14:39:59 -0700

    Clearing out LSB works, next will attempt to store image in the LSB of another, still in the spatial domain.

--- testData/LSB_MSB.m
+++ testData/LSB_MSB.m
@@ -2,10 +2,18 @@
 dave = imread('data\daveg01.bmp');
 dave = imresize( dave , [128 128]);
 seed = imresize( dave , [32 32]);
+shiftAmount = 3;
+
+figure;imshow(seed)
 
 for r=1:32
     for c=1:32
-        seed(r,c)
-        dec2bin(seed(r,c),8)
+        bit = bitshift(seed(r,c),-shiftAmount);
+        bit = bitshift(bit,shiftAmount);
+        seed(r,c) = bit;
+%         seed(r,c)
+%         dec2bin(seed(r,c),8)
     end
 end
+
+figure;imshow(seed)
