0ff7bdb18fa580c59ececfa90aa0027df17f00ef
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-03-28 02:01:44 -0700

    Got some bitshifting and LSB embedding with MSB's but there are some difficulties. One, we must know the length of the bitstring. For smaller values, the data will be lost. I'm still trying to see what I can do about that.

--- testData/LSB_MSB.m
+++ testData/LSB_MSB.m
@@ -4,16 +4,33 @@ dave = imresize( dave , [128 128]);
 seed = imresize( dave , [32 32]);
 shiftAmount = 3;
 
-figure;imshow(seed)
+a = uint8(13);
+s = uint8(10);
 
-for r=1:32
-    for c=1:32
-        bit = bitshift(seed(r,c),-shiftAmount);
-        bit = bitshift(bit,shiftAmount);
-        seed(r,c) = bit;
-%         seed(r,c)
-%         dec2bin(seed(r,c),8)
-    end
+% Erase the bits at the end, not necessary dummy
+% a = bitshift(a,-shiftAmount);
+% a = bitshift(a,shiftAmount);
+
+% Insert shiftAmount MSB into LSB
+msb = 4;
+for lsb=1:shiftAmount
+    a = bitset(a,lsb,bitget(s,msb));
+    msb = msb - 1;
 end
 
-figure;imshow(seed)
+disp(a)
+disp(s)
+
+% figure;imshow(seed)
+
+% for r=1:32
+%     for c=1:32
+%         bit = bitshift(seed(r,c),-shiftAmount);
+%         bit = bitshift(bit,shiftAmount);
+%         seed(r,c) = bit;
+%         seed(r,c)
+%         dec2bin(seed(r,c),8)
+%     end
+% end
+
+% figure;imshow(seed)
