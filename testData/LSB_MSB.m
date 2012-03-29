7513e955e8d952581255d0fc1297cf3eac19ce90
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-03-29 08:39:06 -0700

    Simply adding this test file to the repository so that it can be accessed somewhere else.

--- testData/LSB_MSB.m
+++ testData/LSB_MSB.m
@@ -5,24 +5,27 @@ dave = imresize( dave , [128 128]);
 seed = imresize( dave , [32 32]);
 depth = 3;
 
-% Subtract 128
-dave = dave - 128;
+figure;imshow(dave)
 
 daveOrig = dave;
 seedClone = uint8(size(seed));
-figure;imshow(dave)
+
 
 % Iterate in increments of 8's because we are doing 8x8 blocks
 for r=1:8:size(seed,1)
     for c=1:8:size(seed,2)
+        
+        % MacroBlock
+        mb = single(dave(r:(r+7),c:(c+7))) - 128;
+        
         % Apply DCT to the block before seeding
-        seeded_block = single(dct2(dave(r:(r+7),c:(c+7))));
+        seeded_block = dct2(mb);
         
         % For every block embed part of the image.
         seeded_block = embed(seeded_block, seed(r:(r+7),c:(c+7)),depth );
         
         % Apply I-DCT to the block before applying back to the image
-        seeded_block = idct2(seeded_block);
+        seeded_block = idct2(seeded_block) + 128;
         
         % Apply seed to image
         dave(r:(r+7),c:(c+7)) = seeded_block;
@@ -32,8 +35,8 @@ for r=1:8:size(seed,1)
     end
 end
 
-figure;imshow(dave)
-figure;imshow(uint8(daveOrig - dave ))
+figure;imshow(uint8(dave))
+% figure;imshow(uint8(daveOrig - dave ))
 
-figure;imshow(seed)
-figure;imshow(seedClone)
+% figure;imshow(seed)
+% figure;imshow(seedClone)
