ecc104c984379bd24b4cffc656e770b28de578b0
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-03-30 14:26:01 -0700

    I changed the way embed works, I think I may be able to keep this form for the future even if what I'm going to try wont work.

--- embed.m
+++ embed.m
@@ -1,6 +1,8 @@
 function [ seeded_carrier ] = embed( carrier, seed, depth )
 %EMBED Embeds a matrix into another.
-%   TODO: Detailed explanation goes here
+%   carrier is a value in the frequency domain
+%   seed is a number that needs to be embedded in the carrier value
+%   depth is the number of bits in the seed.
 
 % TODO: Decide on the bit precision
 bit_precision = 8;
@@ -14,45 +16,30 @@ mantissa = carrier - int_carrier;
 % Copying values to seeded carrier
 seeded_carrier = int_carrier;
 
-% Iterate through the elements of the seed matrix
-for r=1:size(seed,1)
-    for c=1:size(seed,2)
-        
-        % Get alias for the current sc position
-        sc = seeded_carrier(r,c);
-        negbit = 0;
-        
-        % Set negbit if negative
-        if sc < 0
-            negbit = 1;
-            sc = abs(sc);
-        end
-        
-        % TODO: Cast to correct type.
-        sc = uint32(sc);
-        
-        % Perform the actual embedding
-        msb = bit_precision; 
-        for lsb=1:depth
-            sc = bitset(sc,lsb,bitget(seed(r,c),msb));
-            msb = msb - 1;
-        end
-        
-        % TODO: Cast back to original carrier type
-        sc = cast(sc,class(carrier));
-        
-        % Unset negbit if it ever was negative
-        if negbit
-            sc = -sc;
-        end
-        
-        % Store value back
-        seeded_carrier(r,c) = sc;
-    end
+% Extract negbit
+negbit = 0;
+if seeded_carrier < 0
+    negbit = 1;
+    seeded_carrier = abs(seeded_carrier);
 end
 
-% Add the mantissa back
-seeded_carrier = seeded_carrier + mantissa;
+seeded_carrier = uint32(seeded_carrier);
+
+% Perform embedding
+msb = depth;
+for lsb=1:depth
+    seeded_carrier = bitset(seeded_carrier,lsb, bitget(seed,msb));
+    msb = msb - 1;
+end
+
+% Casting back to originating class.
+seeded_carrier = cast(seeded_carrier,class(carrier));
+
+% Restoring negbit
+if negbit
+    seeded_carrier = -seeded_carrier;
+end
+
 
 % DEBUG: Printing out the binary just to confirm.
 %disp(dec2bin(seeded_carrier,bit_precision))
