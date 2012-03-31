a7f6b83a2677c70b2d44980c400bb856f5e6866f
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-03-31 16:50:25 -0700

    I think I found a way to make it work with this new form of steg.

--- extract.m
+++ extract.m
@@ -5,32 +5,16 @@ function [ seed ] = extract( seeded_carrier , depth)
 % TODO: Decide on the bit precision
 bit_precision = 8;
 
-% Pre-process seeded carrier
-int_scarrier = uint32(abs(fix(seeded_carrier)));
+% Pre-process seeded carrier, truncate, abs, convert to uint32
+carrier = uint32(abs(fix(seeded_carrier)));
 
-% Initialize seed.
-seed = zeros(size(seeded_carrier));
+% Obtain the last depth bits.
+bitstring = dec2bin(carrier, bit_precision)';
+seedstring = bitstring(bit_precision-depth+1:size(bitstring,1));
 
-% Iterate through carrier
-for r=1:size(int_scarrier,1)
-    for c=1:size(int_scarrier,2)     
-        
-        bitstring = '';
-        
-        % Extract the bits to a certain depth
-        for bit=1:depth
-            bitstring = strcat(bitstring,num2str(bitget(int_scarrier(r,c),bit)));
-        end
-        
-        % TODO: Once I test this and it works, I'll compress to one
-        % statement.
-        % Store the binary value as a decmial.
-        decValue = bin2dec(bitstring);
-        % Must pad the value to matche the precision
-        decValue = bitshift(decValue, bit_precision-depth);
-        seed(r,c) = decValue;
-    end
-end
+% Convert string to a number and pad it to be the correct bit size
+seed = uint8(bin2dec(seedstring'));
+seed = bitshift(seed,bit_precision-depth);
 
 % DEBUG: Printing out the binary just to confirm.
 % disp(dec2bin(seed,bit_precision))
