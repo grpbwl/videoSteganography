698c207652b2c96b5bf2ea4a1e6d4d5703903d90
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-03-31 15:37:10 -0700

    Changed the way it works so that now we can specify if we want the inverse or not.

--- testData/mbdct2.m
+++ testData/mbdct2.m
@@ -1,4 +1,4 @@
-function [ mbdctmatrix ] = mbdct2( matrix )
+function [ mbdctmatrix ] = mbdct2( matrix , inv)
 %MBDCT2 Apply dct2 to every 8x8 macroblock in matrix.
 %   Detailed explanation goes here
 
@@ -6,7 +6,11 @@ mbdctmatrix = size(matrix);
 
 for r=1:8:size(matrix,1)
     for c=1:8:size(matrix,2)
-        mbdctmatrix(r:r+7,c:c+7) = dct2(matrix(r:r+7,c:c+7));
+        if inv
+            mbdctmatrix(r:r+7,c:c+7) = idct2(matrix(r:r+7,c:c+7));
+        else
+            mbdctmatrix(r:r+7,c:c+7) = dct2(matrix(r:r+7,c:c+7));
+        end
     end
 end
 
