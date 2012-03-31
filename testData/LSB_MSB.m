2d53ff958857daa4eb9340270bb22e248f55e813
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-03-31 15:37:31 -0700

    Changed the approach to hiding the information.

--- testData/LSB_MSB.m
+++ testData/LSB_MSB.m
@@ -1,51 +1,56 @@
 % Reading the information and storing it to get rid of the 4 MSB
 % dave = imread('data\daveg01.bmp');
 dave = imread('data/daveg01.bmp');
+seedSize = 32;
 dave = imresize( dave , [128 128]);
-seed = imresize( dave , [16 16]);
+seed = imresize( dave , [seedSize seedSize]);
 depth = 8;
+bp = 1;
 
 % figure;imshow(dave)
 
 daveOrig = dave;
 seedClone = size(seed);
 
+% Apply DCT to dave
+dctDave = mbdct2(dave,0);
 
-% Iterate in increments of 8's because we are doing 8x8 blocks
-for r=1:8:size(seed,1)
-    for c=1:8:size(seed,2)
+% Convert seed and carrier into a 1D array
+dave1d = reshape(dctDave',128*128,1);
+seed1d = reshape(seed',seedSize*seedSize,1);
+
+% Iterate through the seed array
+j = 1;
+for i=1:size(seed1d,1)
+    % Assuming that the seed is int.
+    currentSeed = dec2bin(seed1d(i))';
+    
+    % Itearating from the 1st MSB to the last bit.
+    for bit=1:bp:size(currentSeed,1)
         
-        % Attempt to skip half of the values in the seed.
-        % TODO: This probably goes somewehere else.
-%         if (r + c == 9) % 9 is related to the MB size
-%             continue;
-%         end
+        % Plant the seed
+        seeded_carrier = embed(dave1d(j),bin2dec(currentSeed(1:bp)'),bp);
+        dave1d(j) = seeded_carrier;
         
-        % MacroBlock
-        mb = dave(r:(r+7),c:(c+7));% - 128;
+        % Truncate currentSeed
+        currentSeed = currentSeed(bp+1:size(currentSeed,1));
         
-        % TODO: Consider changing the type to single.
-        % mb = single(mb);
-        
-        % Apply DCT to the block before seeding
-        seeded_block = dct2(mb);
-        
-        % For every block embed part of the image.
-        seeded_block = embed(seeded_block, seed(r:(r+7),c:(c+7)),depth );
-        
-        % Apply I-DCT to the block before applying back to the image
-        seeded_block = idct2(seeded_block);% + 128;
-        
-        % Apply seed to image
-        dave(r:(r+7),c:(c+7)) = seeded_block;
-        
-        % Retrieve
-        seedClone(r:(r+7),c:(c+7)) = extract(dct2(seeded_block), depth);
-    end
+        % Increase counter for carrier's index
+        j = j + 1;
+    end 
 end
+
+% Convert carrier back to 2D
+seeded_dave = reshape(dave1d,128,128)';
+
+% Testing Inverse DCT
+dctTest = seeded_dave;
+
+% Apply IDCT to dave
+seeded_dave = mbdct2(seeded_dave,1);
 
 % figure;imshow(uint8(dave))
 % figure;imshow(uint8(daveOrig - dave ))
 
-figure;imshow(seed)
-figure;imshow(uint8(seedClone))
+% figure;imshow(seed)
+% figure;imshow(uint8(seedClone))
