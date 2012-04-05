c79b3da4c96ea6119cd24ad210792c05fb06da8d
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-04-04 23:26:45 -0700

    From 40seconds to 9secs, DAMN!!

--- extract.m
+++ extract.m
@@ -8,15 +8,15 @@ bit_precision = 8;
 % Pre-process seeded carrier, truncate, abs, convert to uint32
 carrier = uint32(abs(fix(seeded_carrier)));
 
-% Obtain the last depth bits.
-bitstring = dec2bin(carrier, bit_precision)';
-offset = size(bitstring,1) - bit_precision;
-first = bit_precision-depth + 1 + offset;
-last = bit_precision + offset;
-seedstring = bitstring(first:last);
+% % Obtain the last depth bits.
+% bitstring = dec2bin(carrier, bit_precision)';
+% offset = size(bitstring,1) - bit_precision;
+% first = bit_precision-depth + 1 + offset;
+% last = bit_precision + offset;
+% seedstring = bitstring(first:last);
 
 % Convert string to a number and pad it to be the correct bit size
-seed = uint8(bin2dec(seedstring'));
+seed = uint8(bitshift(carrier,0,depth));
 
 % DEBUG: Printing out the binary just to confirm.
 % disp(dec2bin(seed,bit_precision))
