45cb59d6a8ba650a2b087c2a843cb2dafc0ca1ed
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-04-08 13:56:30 -0700

    Adding Aaron's changes to his method of encoding and extracting.

--- desteg.m
+++ desteg.m
@@ -1,11 +1,13 @@
+F = 150;
 extracted=newFileYuv(176,144);
-for frame=1:15
-    [carrier]= loadFileYuv('new_carrier.yuv',352, 288, frame);
+ndctDomain = double(zeros(288,352));
+ninverseDctDomain = double(zeros(288,352));
+unhide=double(zeros(1,50688));
+for frame=1:F
+    tic;
+    [carrier]= loadFileYuv('new_carrier.yuv',352, 288, frame);    
+    scaledImg = double(carrier.cdata(:,:,1));
     
-    scaledImg = double(carrier.cdata(:,:,1));% - 128 ;
-
-        ndctDomain = double(zeros(288,352));
-        ninverseDctDomain = double(zeros(288,352));
         iter=1;
         for r=1:8:288
             for w=1:8:352
@@ -48,16 +50,12 @@ for frame=1:15
                  unhide(iter)=nmb(6,3);iter=iter+1;
                  unhide(iter)=nmb(3,6);iter=iter+1;
                  unhide(iter)=nmb(2,7);iter=iter+1;
-
-                 ndctDomain(r: (r + 7), w:(w+7)) = nmb;
-
                  ninverseDctDomain(r: (r + 7), w:(w+7)) = ( idct(idct(nmb)')');% + 128 );            
             end
         end
-        anothernewImg=uint8(ninverseDctDomain);%(imresize(uint8(inverseDctDomain), [288 352]));
-%         figure;imshow(anothernewImg);
         unhide=uint8(unhide);
 
+    new_im=uint8(zeros(144,176));
     k=1;
     for i=1:1:144
         for j=1:1:176
@@ -65,9 +63,11 @@ for frame=1:15
             k=k+2;
         end
     end
+    %new_im=imfilter(new_im,fspecial('gaussian',176));
 
     extracted(frame).cdata(:,:,1)=new_im;
     extracted(frame).cdata(:,:,2)=new_im;
     extracted(frame).cdata(:,:,3)=new_im;
+    toc;
 end
 saveFileYuv(extracted, 'extracted.yuv', 'w');
