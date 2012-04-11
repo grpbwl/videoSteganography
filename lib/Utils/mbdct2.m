9aaafa2a93f2a54031559dc9d94979892324fb9e
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-04-11 15:09:39 -0700

    Simply added more comments to show what it does.

--- lib/Utils/mbdct2.m
+++ lib/Utils/mbdct2.m
@@ -1,9 +1,10 @@
 function [ mbdctmatrix ] = mbdct2( matrix , inv)
 %MBDCT2 Apply dct2 to every 8x8 macroblock in matrix.
-%   Detailed explanation goes here
+%   Takes in a full matrix, and applies 2D DCT on every 8x8 macro block. It
+%   returns a matrix of the original input size, with 8x8 2D DCT values.
+%   If inv = 1, this algorith will perform 2D IDCT instead of 2D DCT.
 
 mbdctmatrix = size(matrix);
-
 for r=1:8:size(matrix,1)
     for c=1:8:size(matrix,2)
         if inv
