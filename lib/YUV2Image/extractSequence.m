8ad3cefd2e31ff104e429505e4892641985b01e4
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-04-11 22:59:58 -0700

    Adding function that will allow us to extracte a specific part of the sequence. It takes parameters such as the path of the YUV file, range, and format of file.

---
+++ lib/YUV2Image/extractSequence.m
@@ -0,0 +1,34 @@
+function [ sequence ] = extractSequence( yuvPath , a, b, type)
+%EXTRACTSEQUENCE Extracts a specific region of the yuvVideo
+%   Extract a sequence of frames from the YUV video starting from a to b.
+%   The type of the sequence must be specified, it must either be 'cif' or
+%   'qcif'.
+
+% Load format sizes
+load('format');
+sequenceSize = [0 0];
+
+% Checking which size to use, if invalid argument given, bail.
+if strcmp('qcif',type)
+    sequenceSize = format.qcif;
+elseif strcmp('cif', type)
+    sequenceSize = format.cif;
+else
+    error('Invalid value for ''type'', valid options are ''cif'' and ''qcif''');
+end
+
+% Ensuring correct range.
+if a > b
+    error('Invalid range: a must be less than b.');
+end
+
+sequence = repmat(struct('cdata',uint8(zeros(sequenceSize(2),sequenceSize(1),3)),'colormap',cell(1)),1,(b-a));
+
+% Traverse the video sequence extracting all the frames and storing them
+for frame=a:b
+    temp = loadFileYuv(yuvPath,sequenceSize(1),sequenceSize(2),frame);
+    sequence(frame).cdata = temp.cdata;
+end
+
+end
+
