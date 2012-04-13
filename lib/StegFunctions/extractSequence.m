aa12cb1c72241bcb49b1b4d4243074c59cac58e2
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-04-13 01:00:44 -0700

    I'm trying to make it work as a function, but I probably messed something up. Going to be coming up with a better way to approach this.

--- lib/StegFunctions/extractSequence.m
+++ lib/StegFunctions/extractSequence.m
@@ -1,55 +1,55 @@
-%% Retrieve Seed 8 seconds
-qcif = [ 176 144 ] ;
-cif = [ 352 288 ];
-F=5;
-depth = 8;
-bp = 4;
-% Pre-allocating
-seed_sequence = repmat(struct('cdata',uint8(zeros(qcif(2),qcif(1),3)),'colormap',cell(1)),1,F);
+function [ sequence ] = extractSequence( seededSequencePath, frameCount, key, bitPrecision )
+%EXTRACTSEQUENCE Short desc.
+%   Long Desc.
 
-parfor frame=1:F
-    [temp] =  loadFileYuv('trigoman.yuv',cif(1),cif(2),frame);
-    seeded_carrier = rgb2gray(temp.cdata(:,:,:));
+% Loading external constants
+load('format');
+
+% Pre-allocating for the embedded sequence
+sequence = repmat(struct('cdata',uint8(zeros(format.qcif(2),format.qcif(1),3)),'colormap',cell(1)),1,frameCount);
+
+% Get the sequence of frames.
+seededCarrier = extractYuv(seededSequencePath,1,frameCount,'cif');
+
+for frame=1:frameCount
+    
+    % Get current frame and convert 3 channels to grayscale.
+    currentCarrier = rgb2gray(seededCarrier(frame).cdata(:,:,:));
     
     % Convert seed and carrier into a 1D array
-    dctCarrier = mbdct2(seeded_carrier,0);
-    carrier1d = reshape(dctCarrier',cif(1)*cif(2),1);
+    dctCarrier = mbdct2(currentCarrier,0);
+    carrier1d = reshape(dctCarrier',format.cif(1)*format.cif(2),1);
 
     % Create place to hold recovered bits.
-    outputMSB = zeros(qcif(1)*qcif(2),1);
-    outputLSB = zeros(qcif(1)*qcif(2),1);
+    outputMSB = zeros(format.qcif(1)*format.qcif(2),1);
 
     msb = 1; lsb = 1;
     for i=1:size(carrier1d,1)
         if( key(i) == 1 )
             % I have 4 bits
-            hword = extract(carrier1d(i),bp);
+            extractedValue = extract(carrier1d(i),bitPrecision);
 
             % Inserting in the correct place
             if msb <= lsb
-                outputMSB(msb) = hword;
+                outputMSB(msb) = extractedValue;
                 msb = msb + 1;
             else
-                outputMSB(lsb) = bitshift(outputMSB(lsb),bp);
-                outputMSB(lsb) = bitor(outputMSB(lsb),hword);  
-    %             outputLSB(lsb) = hword;
+                outputMSB(lsb) = bitshift(outputMSB(lsb),format.bitDepth-bitPrecision,format.bitDepth);
+                outputMSB(lsb) = bitor(outputMSB(lsb),extractedValue);  
                 lsb = lsb + 1;
-
-                % Consider joining here.
             end
 
-            if lsb > size(outputLSB,1)
+            if lsb > size(outputMSB,1)
                 break;
             end
         end
     end
 
     % Resize output
-    output = im2uint8(ind2rgb(reshape(outputMSB,qcif(1),qcif(2))',gray(256)));
+    output = im2uint8(ind2rgb(reshape(outputMSB,format.qcif(1),format.qcif(2))',gray(256)));
     
     % Store in YUV Sequence
-    seed_sequence(frame).cdata = output;
+    sequence(frame).cdata = output;
 end
 
-% Save the sequence
-saveFileYuv(seed_sequence,'extracted_sequence.yuv','w');
+end
