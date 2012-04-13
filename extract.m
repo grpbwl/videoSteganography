cc277a014e762a0f14d96c77c8de46407c058427
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-04-13 12:56:46 -0700

    Attempting to fix extract, I feel like this one will work.

--- extract.m
+++ extract.m
@@ -5,12 +5,12 @@ function [ seed ] = extract( seededCarrier , bitPrecision, bitDepth, lsbFlag)
 % Pre-process seeded carrier, truncate, abs, convert to uint32
 carrier = uint32(abs(fix(seededCarrier)));
 
-% Convert string to a number and pad it to be the correct bit size
-if lsbFlag
-    seed = uint8(bitshift(carrier,0,bitPrecision));
-else
-    seed = uint8(bitshift(carrier,bitDepth-bitPrecision,bitPrecision));
+% Extract the bits that we need, that is get the last bitPrecision bits.
+seed = uint8(bitshift(carrier,0,bitPrecision));
+
+% If msb shift accordingly.
+if lsbFlag == 0
+    seed = bitshift(seed,bitDepth-bitPrecision,bitDepth);
 end
 
 end
-
