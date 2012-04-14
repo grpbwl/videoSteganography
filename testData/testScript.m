bd9e750ff7a8e11d2ee23c90c4988995a07748c6
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-04-14 13:25:18 -0700

    Added key.mat which contains constant key. Changed the location of extract.m and embed.m.

--- testData/testScript.m
+++ testData/testScript.m
@@ -1,12 +1,16 @@
 %% Parameters
-f = 1;
+f = 15;
 bp = 5;
 clear key;
 load('key');
 %% Embed the seed image.
+tic;
 [s,k] = embedSequence('bus_cif.yuv','foreman_qcif.yuv',f,key,bp);
 saveFileYuv(s,'test.yuv',1,0);
+toc;
 
 %% Extract embedded frame.
+tic;
 o = extractSequence('test.yuv',f,k,bp);
 saveFileYuv(o,'seed.yuv',1,1);
+toc;
