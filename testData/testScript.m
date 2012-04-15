2af408f6d969e0f8fd301b7083dd9733e0c8e604
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-04-15 14:04:47 -0700

    Added results images.

--- testData/testScript.m
+++ testData/testScript.m
@@ -1,16 +1,13 @@
 %% Parameters
-f = 15;
-bp = 5;
+f = 1;
+bp = 6;
 clear key;
 load('key');
 %% Embed the seed image.
-tic;
 [s,k] = embedSequence('bus_cif.yuv','foreman_qcif.yuv',f,key,bp);
 saveFileYuv(s,'test.yuv',1,0);
-toc;
+
 
 %% Extract embedded frame.
-tic;
 o = extractSequence('test.yuv',f,k,bp);
 saveFileYuv(o,'seed.yuv',1,1);
-toc;
