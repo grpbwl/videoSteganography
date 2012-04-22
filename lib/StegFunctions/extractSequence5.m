d1db681539bfa5e418d2740b356b0324d0d8b70f
Jose V. Trigueros <j.v.trigueros@gmail.com>
2012-04-22 14:45:46 -0700

    Been trying a bunch of things, need to race the clock!

--- lib/StegFunctions/extractSequence5.m
+++ lib/StegFunctions/extractSequence5.m
@@ -1,32 +1,84 @@
-function [ sequence ] = extractSequence( seededSequencePath, frameCount, key, bitPrecision )
+function [ sequence ] = extractSequence5( seededSequencePath, frameCount, key, bitPrecision )
 %EXTRACTSEQUENCE Short desc.
 %   Long Desc.
 
 % Loading external constants
 load('format');
 fDepth = 5;
+cif = format.cif;
+qcif = format.qcif;
+bitDepth = format.bitDepth;
+fDepth = 5;
+
+% Force bitPrecision to 1
+bitPrecision = 1;
+
 % Pre-allocating for the embedded sequence
-sequence = repmat(struct('cdata',uint8(zeros(format.qcif(2),format.qcif(1),3)),'colormap',cell(1)),1,frameCount);
+sequence = repmat(struct('cdata',uint8(zeros(qcif(2),qcif(1),3)),'colormap',cell(1)),1,frameCount);
 
 % Get the sequence of frames.
-seededCarrier = extractYuv(seededSequencePath,1,frameCount*5,'cif');
+carrier = extractYuv(seededSequencePath,1,frameCount*5,'cif');
 % Create place to hold recovered bits.
-outputBits = zeros(format.qcif(1)*format.qcif(2),1);
+outputBits = zeros(qcif(1)*qcif(2),1);
 
-seedFrame = 1;
-lsb = 0;
-frame = 1;
-for carrierFrame=fDepth*frame-(fDepth-1):carrierFrame*frame
-        
-    bit = extract(seededCarrier(carrierFrame)
+for frame=1:frameCount
+    %% Read current image and convert to grayscale.
+%     % Get all three channels and convert to true grayscale.
+%     slicedSeed = seed(frame);
+%     currentSeed = slicedSeed.cdata(:,:,1); % Getting Y-Channel
     
-    % Change the lsb bit.
-    if( mod(carrierFrame,5) == 0 )
-        lsb = 1;
-    else 
-        lsb = 0;
+    % Pre-allocate memory for carriers
+%     carriers = repmat(struct('data',zeros(cif(1),cif(2),3)),fDepth,1);
+    carriersDCT = repmat(struct('data',zeros(cif(1)*cif(2),1)),fDepth,1);
+    
+    % Get the next 5 valid frames and store them in dct form.
+    carrierFrame = fDepth*frame-(fDepth-1);
+    for i=1:fDepth
+%         carriers(i).data= carrier(carrierFrame).cdata; % Getting YUV-Channels
+        carriersDCT(i).data = reshape(mbdct2(carrier(carrierFrame).cdata(:,:,1)',0),cif(1)*cif(2),1); % Getting DCT of Y channel.
+        carrierFrame = carrierFrame + 1;
     end
     
+    i = 1;
+    msb = 1;bitCounter = 1;
+    while( i <= size(key,1) )
+        if( key(i) == 1 )
+        
+            temp = uint8(0);
+            % Do the bottom 5 times
+            for k=1:5
+                extracted = extract(carriersDCT(k).data(i),bitPrecision,bitDepth,1);
+                % Bitshift the current bit one to the right and perform an
+                % OR operation with the upcoming bit.
+                temp = bitor(bitshift(temp,1),extracted);
+                % Reverse bit order. TODO: Must check if the bits are
+                % correct.
+            end
+            
+            if msb == 1
+                outputBits(bitCounter) = bitrevorder(temp);
+                msb = 0;
+            else
+                outputBits(bitCounter) = bitor(outputBits(bitCounter),temp);
+                bitCounter = bitCounter + 1;
+                msb = 1;
+            end
+            
+            % Bail if i is bigger than the size of the output bits.
+            if bitCounter > size(outputBits,1)
+                break;
+            end
+        end
+        i = i + 1;
+        
+
+    end
+    
+    % Resize and format output
+    output = im2uint8(ind2rgb(reshape(outputBits,qcif(1),qcif(2))',gray(256)));
+    
+    %% Put everything back from those 5 frames.
+    sequence(frame).cdata = output;
 end
 
 end
